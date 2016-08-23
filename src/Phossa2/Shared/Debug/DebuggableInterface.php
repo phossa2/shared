1e94e442c5ac572fac7cf7a4fe4c89082e42d74b
Hong Zhang <phossa@126.com>
2016-08-23 09:54:30 +0800

    added debuggable

---
+++ src/Phossa2/Shared/Debug/DebuggableInterface.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Phossa Project
+ *
+ * PHP version 5.4
+ *
+ * @category  Library
+ * @package   Phossa2\Shared
+ * @copyright Copyright (c) 2016 phossa.com
+ * @license   http://mit-license.org/ MIT License
+ * @link      http://www.phossa.com/
+ */
+/*# declare(strict_types=1); */
+
+namespace Phossa2\Shared\Debug;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * DebuggableInterface
+ *
+ * @package Phossa2\Shared
+ * @author  Hong Zhang <phossa@126.com>
+ * @version 2.0.26
+ * @since   2.0.26 added
+ */
+interface DebuggableInterface
+{
+    /**
+     * Enable debugging
+     *
+     * @param  bool $flag
+     * @return $this
+     * @access public
+     * @api
+     */
+    public function enableDebug(/*# bool */ $flag = true);
+
+    /**
+     * Set the logger
+     *
+     * @param  LoggerInterface $logger
+     * @return $this;
+     * @access public
+     * @api
+     */
+    public function setDebugger(LoggerInterface $logger);
+
+    /**
+     * Send debug message
+     *
+     * @param  string $message
+     * @return $this
+     * @access public
+     */
+    public function debug(/*# string */ $message);
+}
---
+++ src/Phossa2/Shared/Debug/DebuggableTrait.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Phossa Project
+ *
+ * PHP version 5.4
+ *
+ * @category  Library
+ * @package   Phossa2\Shared
+ * @copyright Copyright (c) 2016 phossa.com
+ * @license   http://mit-license.org/ MIT License
+ * @link      http://www.phossa.com/
+ */
+/*# declare(strict_types=1); */
+
+namespace Phossa2\Shared\Debug;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * DebuggableTrait
+ *
+ * @package Phossa2\Shared
+ * @author  Hong Zhang <phossa@126.com>
+ * @see     DebuggableInterface
+ * @version 2.0.26
+ * @since   2.0.26 added
+ */
+trait DebuggableTrait
+{
+    /**
+     * @var    bool
+     * @access protected
+     */
+    protected $debug_mode = false;
+
+    /**
+     * @var    LoggerInterface
+     * @access protected
+     */
+    protected $debug_logger;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function enableDebug(/*# bool */ $flag = true)
+    {
+        $this->debug_mode = (bool) $flag;
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setDebugger(LoggerInterface $logger)
+    {
+        $this->debug_logger = $logger;
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function debug(/*# string */ $message)
+    {
+        if ($this->debug_mode && $this->debug_logger) {
+            $this->debug_logger->debug(
+                $message,
+                ['className' => get_class($this)]
+            );
+        }
+        return $this;
+    }
+}
